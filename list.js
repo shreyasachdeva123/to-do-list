475635c2f8dd341f2ff85fd509ed27fd4727c1e1
Shreya Sachdeva <sachdeva.shreya@gmail.com>
2021-04-29 11:31:11 -0700

    Fixed delete buttons for each li

--- list.js
+++ list.js
@@ -1,27 +1,21 @@
-let addBtn = document.getElementById("addBtn");
-let clearBtn = document.getElementById("clear");
-let ul = document.getElementById("list_items");
+var addButton = document.getElementById("addBtn");
+var clearBtn = document.getElementById("clear");
+var ul = document.getElementById("list_items");
 
-addBtn.addEventListener("click", () => {
-    let li = document.createElement("li");
-    li.innerHTML = `${input_field.value}`;
-    let closeBtn = document.createElement("button");
-    closeBtn.className = "close";
-    closeBtn.innerText = "Delete";
-    li.appendChild(closeBtn);
+addButton.addEventListener("click", () => {
+    var li = document.createElement("li");
+    li.innerHTML = `${input_field.value}<button class="close">Delete</button>`;
     ul.appendChild(li);
     input_field.value = "";
+    var deleteButton = document.getElementsByClassName("close");
+    console.log(deleteButton);
+    for (var i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener("click", (e) => {
+            e.target.style.visibility = "hidden";
+            e.target.parentElement.style.visibility = "hidden";
+        });
+    }
 });
-
-
-let closeBtn = document.createElement("button");
-closeBtn.className = "close";
-closeBtn.innerText = "Delete";
-
-li.appendChild(closeBtn);
-
-
-
 
 clearBtn.addEventListener("click", () => {
     document.getElementById("list_items").textContent = "";
