5a3883b7921f311eb52a053a5e909e0d26f3e6ba
Shreya Sachdeva <sachdeva.shreya@gmail.com>
2021-05-04 15:34:49 -0700

    Added Done button

--- list.js
+++ list.js
@@ -1,6 +1,7 @@
 var addButton = document.getElementById("addBtn");
 var clearBtn = document.getElementById("clear");
 var ul = document.getElementById("list_items");
+var doneButton = document.getElementsByClassName("complete");
 
 addButton.addEventListener("click", (e) => {
     fetch("http://todo-backend-sinatra.herokuapp.com/todos", {
@@ -11,35 +12,45 @@ addButton.addEventListener("click", (e) => {
         .then(response => response.json())
         .then(json => console.log(json));
     var li = document.createElement("li");
-    li.innerHTML = `<p>${input_field.value}</p><button class="close">Delete</button>`;
+    li.innerHTML = `<p>${input_field.value}</p><button class="complete">Done</button><button class="close">Delete</button>`;
     ul.appendChild(li);
     input_field.value = "";
     var deleteButton = document.getElementsByClassName("close");
 });
+
 
 ul.addEventListener("click", (e) => {
     if (e.target.className === `close`) {
         console.log(e.target.dataset);
         fetch(e.target.dataset.url, { method: "DELETE" })
             .then(() => ul.removeChild(e.target.parentElement))
-            .catch(error => console.log("OOPSY", error))
+            .catch(error => console.log("oopsy", error))
+    } else if (e.target.className === "complete") {
+        console.log(e.target.dataset.url);
+        fetch(e.target.dataset.url, {
+                method: "PUT",
+                body: JSON.stringify({ completed: true }),
+                headers: { "content-type": "application/json" }
+            })
+            .then(response => console.log(response))
+            .then(() => e.target.previousElementSibling.innerHTML.strike())
+            .catch(error => alert("oopsy", error))
     }
 });
 
-fetch('http://todo-backend-sinatra.herokuapp.com/todos')
+fetch('http://todo-backend-sinatra.herokuapp.com/todos', { method: "GET" })
     .then(response => response.json())
     .then(data => generateHTML(data))
 
 function generateHTML(data) {
     console.log(data);
-    const html = data.map(list => {
+    const html = data.map(item => {
         var li = document.createElement("li");
-        li.innerHTML = `<p>${list.title}</p><button data-url="${list.url}" class="close">Delete</button>`;
+        li.innerHTML = `<p>${item.title}</p><button data-url="${item.url}" class="complete">Done</button><button data-url="${item.url}" class="close">Delete</button>`;
         ul.appendChild(li);
         input_field.value = "";
         var deleteButton = document.getElementsByClassName("close");
     }).join("");
-
 }
 
 clearBtn.addEventListener("click", () => {
