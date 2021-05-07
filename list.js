bd9c3ece9cbf05f687e325d58bbc64d31994449f
Shreya Sachdeva <sachdeva.shreya@gmail.com>
2021-05-07 16:31:54 -0700

    Fixed Done Button

--- list.js
+++ list.js
@@ -1,59 +1,85 @@
 var addButton = document.getElementById("addBtn");
 var clearBtn = document.getElementById("clear");
 var ul = document.getElementById("list_items");
-var doneButton = document.getElementsByClassName("complete");
 
 addButton.addEventListener("click", (e) => {
+    console.log(ul.firstChild.dataset.order);
     fetch("https://todo-backend-sinatra.herokuapp.com/todos", {
             method: "POST",
-            body: JSON.stringify({ title: `${input_field.value}` }),
+            body: JSON.stringify({ title: `${input_field.value}`, completed: false, order: ul.firstChild.dataset.order + 1 }),
             headers: { "content-type": "application/json" }
         })
         .then(response => response.json())
-        .then(json => console.log(json));
-    var li = document.createElement("li");
-    li.innerHTML = `<p>${input_field.value}</p><button class="complete">Done</button><button class="close">Delete</button>`;
-    ul.appendChild(li);
-    input_field.value = "";
-    var deleteButton = document.getElementsByClassName("close");
+        .then((data) => {
+            var li = document.createElement("li");
+            li.innerHTML = `<p>${data.title}</p><button data-order="${data.order}" data-completed="${data.completed}" data-url="${data.url}" class="complete">Done</button><button data-url="${data.url}" class="close">Delete</button>`;
+            ul.appendChild(li);
+            input_field.value = "";
+            console.log(data.order);
+        })
+        .catch(error => alert("oopsy", error))
 });
 
 
 ul.addEventListener("click", (e) => {
     if (e.target.className === `close`) {
-        console.log(e.target.dataset);
+        console.log(e.target.dataset.url);
         fetch(e.target.dataset.url, { method: "DELETE" })
             .then(() => ul.removeChild(e.target.parentElement))
-            .catch(error => console.log("oopsy", error))
-    } else if (e.target.className === "complete") {
-        console.log(e.target.dataset.url);
-        fetch(e.target.dataset.url, {
-                method: "PATCH",
-                body: JSON.stringify({ completed: true }),
-                headers: { "content-type": "application/json" }
-            })
-            .then(response => console.log(response))
-            .then(() => e.target.previousElementSibling.innerHTML.strike())
             .catch(error => alert("oopsy", error))
+    } else if (e.target.className === "complete") {
+        console.log(e.target.dataset.completed);
+        if (e.target.dataset.completed === "false") {
+            console.log("1");
+            fetch(e.target.dataset.url, {
+                    method: "PATCH",
+                    body: JSON.stringify({ completed: true }),
+                    headers: { "content-type": "application/json" }
+                })
+                .then(response => console.log(response))
+                .then(() => { e.target.previousElementSibling.style.textDecoration = "line-through"; })
+                .then(() => { e.target.dataset.completed = "true" })
+                .catch(error => alert("oopsy", error))
+        } else {
+            console.log("2");
+            fetch(e.target.dataset.url, {
+                    method: "PATCH",
+                    body: JSON.stringify({ completed: false }),
+                    headers: { "content-type": "application/json" }
+                })
+                .then(response => console.log(response))
+                .then(() => { e.target.dataset.completed = "false" })
+                .then(() => { e.target.previousElementSibling.style.textDecoration = "none"; })
+                .catch(error => alert("oopsy", error))
+        }
     }
 });
 
 fetch('https://todo-backend-sinatra.herokuapp.com/todos', { method: "GET" })
     .then(response => response.json())
     .then(data => generateHTML(data))
-    .finally(() => document.getElementById("load").style.display = "none")
+    .then(() => document.getElementById("load").style.display = "none")
+
 
 function generateHTML(data) {
     console.log(data);
     const html = data.map(item => {
         var li = document.createElement("li");
-        li.innerHTML = `<p>${item.title} ${item.completed}</p><button data-url="${item.url}" class="complete">Done</button><button data-url="${item.url}" class="close">Delete</button>`;
+        li.innerHTML = `<p>${item.title}</p><button data-order="${item.order}" data-completed="${item.completed}" data-url="${item.url}" class="complete">Done</button><button data-url="${item.url}" class="close">Delete</button>`;
         ul.appendChild(li);
         input_field.value = "";
-        var deleteButton = document.getElementsByClassName("close");
+        console.log(item.completed);
+        if (item.completed === true) {
+            console.log("my vaue is true");
+            li.firstChild.style.textDecoration = "line-through";
+        } else {
+            console.log("my value is false");
+            li.firstChild.style.textDecoration = "none";
+        }
     }).join("");
 }
 
 clearBtn.addEventListener("click", () => {
-    document.getElementById("list_items").textContent = "";
+    fetch("https://todo-backend-sinatra.herokuapp.com/todos", { method: "DELETE" })
+        .then(() => { return document.getElementById("list_items").textContent = ""; })
 });
